14889c309c9230fa3fc927438b7db7380bf1cdf7
Jaylaelike <smarkwisai@gmail.com>
2019-08-23 20:44:17 +0700

    Add files via upload

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'a7061bcc-7b7c-4f94-afd4-e63fd7d5f966'; // LED, Button
+const USER_SERVICE_UUID         = '30c7c427-9d3f-48bc-8a9f-fd6618d3e8f9'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
